aa998596f8606619b5008ec2bdb6ad97ca71f00f
Nishit <1407162@EEE1407203.EEE1407203>
2020-07-21 22:03:01 +0530

    some editing

--- day-5/solution5.sh
+++ day-5/solution5.sh
@@ -10,3 +10,4 @@ echo | awk '{printf "%0.2f\n",(42/3)}'
    b= echo | awk '{ printf "%0.02f\n", (40 / 3.281); }'
    Area=echo | awk '{ printf "%0.02f\n", ( (60/ 3.281) * (40 / 3.281) ); }'
    echo $Area
+   echo $(($Area*25))
